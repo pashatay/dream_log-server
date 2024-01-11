26bd443410891047ed06a4bb180e3416c7edf190
pashatay <pashabonilla@gmail.com>
2024-01-10 23:59:28 -0500

    changing config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL:
-    process.env.DATABASE_URL || "postgresql://postgres@localhost/dreamlog",
-  TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL ||
-    "postgresql://postgres@localhost/dreamlog-test",
-  JWT_KEY: process.env.JWT_KEY || "Newyork",
-  EMAIL_PASSWORD: process.env.EMAIL_PASSWORD || "76Jy8.9Ioo",
+  NODE_ENV: process.env.NODE_ENV,
+  DATABASE_URL: process.env.DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+  JWT_KEY: process.env.JWT_KEY,
+  EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
 };
