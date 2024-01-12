c820c7dd88eea2972ccd40c991c07372916a2ce8
pashatay <pashabonilla@gmail.com>
2024-01-11 23:26:40 -0500

    changing congif

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,9 +3,5 @@ require("dotenv").config();
 module.exports = {
   migrationsDirectory: "migrations",
   driver: "pg",
-  connectionString:
-    process.env.NODE_ENV === "test"
-      ? process.env.TEST_DATABASE_URL
-      : process.env.DATABASE_URL,
-  ssl: process.env.SSL ? { rejectUnauthorized: false } : null,
+  connectionString: process.env.DATABASE_URL,
 };
